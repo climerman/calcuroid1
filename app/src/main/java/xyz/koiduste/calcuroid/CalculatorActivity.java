dadaa1be503e896827d1306645aff01991a189bc
Marko Koiduste <mkoiduste@gmail.com>
2016-03-10 13:53:31 +0200

    Kalkulaatori esialgne service tehtud(tehted). Samuti omistan nupud activity initializeris.

--- app/src/main/java/xyz/koiduste/calcuroid/CalculatorActivity.java
+++ app/src/main/java/xyz/koiduste/calcuroid/CalculatorActivity.java
@@ -2,12 +2,54 @@ package xyz.koiduste.calcuroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mCalcInputDisplay;
+    private TextView mCalcOutputDisplay;
+    private CalculatorService mCalcService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        //Service & Display
+        mCalcService = new CalculatorServiceImpl();
+        mCalcInputDisplay = (TextView) findViewById(R.id.textInput);
+        mCalcOutputDisplay = (TextView) findViewById(R.id.textOutput);
+
+        //Buttons
+        findViewById(R.id.button0).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        findViewById(R.id.buttonAdd).setOnClickListener(this);
+        findViewById(R.id.buttonSubtract).setOnClickListener(this);
+        findViewById(R.id.buttonMultiply).setOnClickListener(this);
+        findViewById(R.id.buttonDivide).setOnClickListener(this);
+        findViewById(R.id.buttonEquals).setOnClickListener(this);
+        findViewById(R.id.buttonClear).setOnClickListener(this);
+        findViewById(R.id.buttonDelete).setOnClickListener(this);
+        findViewById(R.id.buttonBrackets).setOnClickListener(this);
+        findViewById(R.id.buttonComma).setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/xyz/koiduste/calcuroid/CalculatorService.java
@@ -0,0 +1,11 @@
+package xyz.koiduste.calcuroid;
+
+/**
+ * Created by marko on 3/9/16.
+ */
+public interface CalculatorService {
+    double add(double num1, double num2);
+    double subtract(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2);
+}
