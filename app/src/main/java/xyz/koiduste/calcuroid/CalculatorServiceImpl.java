22cd064f6e56445b4f57a85e07cac76c8f479b21
Marko Koiduste <mkoiduste@gmail.com>
2016-03-11 16:57:03 +0200

    Kalkulaator peaks nüüd korrektselt töötama. Testisin nipet näpet manuaalselt. Automaatteste ilmselt ei hakka higistama. Databinding pole ka veel alt vedanud, kuigi millegipärast mu boolean ei bindi ära komanupule. No idea why.
    TODO:
    *Layout külili
    *Stiilid
    *Indikaator, mis operaator(tehe) parasjagu aktiivne on. Teostan kas stiiliga(st vajutatud nupp jääb teistest erinev) või lisa textview, mis näitab mis tehe pooleli on.
    *Sulgude süsteem(hetkel see nupp ei funka)

--- app/src/main/java/xyz/koiduste/calcuroid/CalculatorServiceImpl.java
+++ app/src/main/java/xyz/koiduste/calcuroid/CalculatorServiceImpl.java
@@ -49,43 +49,61 @@ public class CalculatorServiceImpl extends MathServiceImpl implements Calculator
 
     @Override
     public void calcOperation(String operator) {
+        commaEnabled.set(true);
         switch(operator) {
             case ADD:
                 resultText.set(String.valueOf(add(getResult(), getOperand())));
+                operandText.set("");
                 break;
             case SUBTRACT:
                 resultText.set(String.valueOf(subtract(getResult(), getOperand())));
+                operandText.set("");
                 break;
             case MULTIPLY:
                 resultText.set(String.valueOf(multiply(getResult(), getOperand())));
+                operandText.set("");
                 break;
             case DIVIDE:
                 resultText.set(String.valueOf(divide(getResult(), getOperand())));
-                break;
-            case EQUALS:
-                break;
-            case CLEAR:
-                //TODO Clear logic
-                break;
-            case BRACKETS:
-                //TODO Brackets logic
-                break;
-            case DELETE:
-                //TODO Delete logic
+                operandText.set("");
                 break;
             case "":
-                resultText.set(operandText.get());
+                if (!operandText.get().isEmpty()) {
+                    resultText.set(operandText.get());
+                }
+                operandText.set("");
                 break;
             default:
+                //throw new RuntimeException("Tundmatu operaator!");
                 //TODO Error handling
                 break;
         }
+        //TODO operandText.set(""); //maybex
     }
 
     @Override
     public void opAction(String key) {
-        calcOperation(waitingOperator);
-        operandText.set("");
-        waitingOperator=key;
+        switch (key) {
+            case EQUALS:
+                calcOperation(waitingOperator);
+                operandText.set("");
+                waitingOperator = "";
+                break;
+            case CLEAR:
+                operandText.set("");
+                resultText.set("");
+                break;
+            case DELETE:
+                operandText.set("");
+                break;
+            case BRACKETS:
+                break;
+            default:
+                if (!waitingOperator.equals(EQUALS)) {
+                    calcOperation(waitingOperator);
+                }
+                waitingOperator = key;
+                break;
+        }
     }
 }
